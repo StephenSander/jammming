bfebb4df2db93630cf02195e0923728bf074b554
Stephen S <stephenmsander@yahoo.com>
2026-03-25 17:10:42 -0700

    Initial styling

--- src/Track.jsx
+++ src/Track.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import styles from './styles/Track.module.css';
 
 function Track(props) {
   console.log(props.track);
 
   return (
-    <div>
-      <p>{props.track.name}</p>
+    <div className={styles.cardContainer}>
+        <p>{props.track.name}</p>
 
-      <button onClick={() => props.onAction(props.track)}>
-        {props.isRemoval ? "-" : "+"}
-      </button>
+        <button onClick={() => props.onAction(props.track)}>
+            {props.isRemoval ? "-" : "+"}
+        </button>
     </div>
   );
 }
--- src/Tracklist.jsx
+++ src/Tracklist.jsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import Track from './Track';
+import styles from './styles/Tracklist.module.css';
 
 function Tracklist(props) {
 
   return (
-    <>
+    <div className={styles.tracklistContainer}>
       {props.tracks?.map(track => (
         <Track
           key={track.id}
@@ -13,7 +14,7 @@ function Tracklist(props) {
           isRemoval={props.isRemoval}
         />
       ))}
-    </>
+    </div>
   );
 };
 
